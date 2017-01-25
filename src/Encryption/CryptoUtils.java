e07988c3a934533b004b734a27e8976d0af2d0ba
ChuaMK254 <mingkiang97@gmail.com>
2017-01-26 00:56:00 +0800

    Change to AES, the padding problem need use base64

--- src/Encryption/CryptoUtils.java
+++ src/Encryption/CryptoUtils.java
@@ -1,15 +1,18 @@
 package Encryption;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.IvParameterSpec;
+import java.security.Key;
 import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
 import java.security.spec.KeySpec;
 
-import org.apache.commons.codec.binary.Base64;
+//import org.apache.commons.codec.binary.Base64;
+
 
 /**
  * Created by tanzh on 25/01/2017.
@@ -24,12 +27,12 @@ public class CryptoUtils {
     static AlgorithmParameterSpec paramSpec;
 
     public CryptoUtils() throws Exception {
-        String myEncryptionKey = "ThisIsSpartaThisIsSparta";
-        byte[] arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
-        KeySpec ks = new DESedeKeySpec(arrayBytes);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance(DESEDE_ENCRYPTION_SCHEME);
-        cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
-        key = skf.generateSecret(ks);
+
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        key = keyGenerator.generateKey();
+
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
 
         SecureRandom random = new SecureRandom();
         byte[] iv = new byte[8];
@@ -40,24 +43,24 @@ public class CryptoUtils {
 
     public static String encrypt(String unencryptedString) {
         String encryptedString = null;
+        byte[] cipherText = null;
         try {
-            cipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
             byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
-            byte[] encryptedText = cipher.doFinal(plainText);
-            encryptedString = new String(Base64.encodeBase64(encryptedText));
+            cipherText = cipher.doFinal(plainText);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return encryptedString;
+        return String.valueOf(cipherText);
     }
 
 
     public static String decrypt(String encryptedString) {
         String decryptedText = null;
         try {
-            cipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
-            byte[] encryptedText = Base64.decodeBase64(encryptedString.getBytes());
-            byte[] plainText = cipher.doFinal(encryptedText);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] enstring = encryptedString.getBytes(UNICODE_FORMAT);
+            byte[] plainText = cipher.doFinal(enstring);
             decryptedText = new String(plainText);
         } catch (Exception e) {
             e.printStackTrace();
@@ -77,5 +80,20 @@ public class CryptoUtils {
         System.out.println("Encrypted String: " + encryptMsg);
         System.out.println("Decrypted String: " + decryptMsg);
 
+    }
+
+    public static String asHex (byte buf[]) {
+
+        //Obtain a StringBuffer object
+        StringBuffer strbuf = new StringBuffer(buf.length * 2);
+        int i;
+
+        for (i = 0; i < buf.length; i++) {
+            if (((int) buf[i] & 0xff) < 0x10)
+                strbuf.append("0");
+            strbuf.append(Long.toString((int) buf[i] & 0xff, 16));
+        }
+        // Return result string in Hexadecimal format
+        return strbuf.toString();
     }
 }
